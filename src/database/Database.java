ae6e9e0320baebbaf0429585c619990b1518960b
Tatetaylor <tatewtaylor@yahoo.com>
2017-12-21 17:06:20 -0700

    This is a test.  First upload to git. start file is what I currently have for a script to create the database

---
+++ src/database/Database.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tatewtaylor
+ */
+public class Database {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Connection connObj = null;
+        Statement stateObj = null;
+        ResultSet resultObj = null;
+        
+        
+      
+    }
+    
+}
