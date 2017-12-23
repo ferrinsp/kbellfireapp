03c7fa22aa04d6c0d022480d7d18a1edc497e09d
Tatetaylor <tatewtaylor@yahoo.com>
2017-12-23 09:41:16 -0700

    Added a basic query and connection to DB and changed the name for a package

--- src/database/Database.java
+++ src/database/Database.java
@@ -8,6 +8,7 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -27,6 +28,29 @@ public class Database {
         Connection connObj = null;
         Statement stateObj = null;
         ResultSet resultObj = null;
+        ResultSetMetaData meta = null;
+        String query = "Select * from user";
+        try {
+            //use your own username and login for the second and third parameters..I'll change this in the future to be dynamic
+            connObj = DriverManager.getConnection("jdbc:mysql://localhost:3306/kbell?useSSL=false", "admin", "1qaz2wsx");
+            stateObj = connObj.createStatement();
+            resultObj = stateObj.executeQuery(query);
+            meta = resultObj.getMetaData();
+            int columns= meta.getColumnCount();
+            System.out.println(columns);
+            for (int i=1; i<=columns;i++){
+                 System.out.print(meta.getColumnName(i)+"\t");
+            }
+            System.out.println();
+            while(resultObj.next()){
+                for (int i=1; i<=columns;i++){
+                     System.out.print(resultObj.getObject(i));
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         
         
       
--- src/gui/Login.java
+++ src/gui/Login.java
@@ -9,12 +9,12 @@ package gui;
  *
  * @author tatewtaylor
  */
-public class gui extends javax.swing.JFrame {
+public class Login extends javax.swing.JFrame {
 
     /**
-     * Creates new form gui
+     * Creates new form for login
      */
-    public gui() {
+    public Login() {
         initComponents();
     }
 
@@ -132,21 +132,23 @@ public class gui extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(gui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new gui().setVisible(true);
+                new Login().setVisible(true);
             }
         });
     }
