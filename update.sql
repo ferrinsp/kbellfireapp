99589da6c8857641459e73fda48da296cc918bc5
Tatetaylor <tatewtaylor@yahoo.com>
2018-01-27 11:27:01 -0700

    One more change.

--- update.sql
+++ update.sql
@@ -353,7 +353,7 @@ LOAD DATA local INFILE 'C:/temp/category.csv'
  ENCLOSED BY '"'
  ESCAPED BY ''
  LINES TERMINATED BY '\n'
- (description);
+ (category_ID, description);
  
  -- Run this for product descriptions
   LOAD DATA local INFILE 'C:/temp/prodDesc.csv'
